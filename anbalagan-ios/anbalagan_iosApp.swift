488c26657995b3c985c5133358b752acaef2209b
anbu <anbu@thelaunchclub.in>
2023-10-19 15:00:09 +0530

    initial commit

---
+++ anbalagan-ios/anbalagan_iosApp.swift
@@ -0,0 +1,33 @@
+//
+//  anbalagan_iosApp.swift
+//  anbalagan-ios
+//
+//  Created by Anbu p on 19/10/23.
+//
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct anbalagan-iosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
